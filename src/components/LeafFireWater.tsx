42f7a7dad401e012223eae38ab27c24133a01a4c
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-22 14:13:12 -0800

    🍃 testing fix: removing lint errors

--- src/components/LeafFireWater.tsx
+++ src/components/LeafFireWater.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ConnectButton } from "thirdweb/react";
 import { client } from "../client";
+import { inAppWallet } from "thirdweb/wallets";
 
 
 type Choice = 'Leaf' | 'Fire' | 'Water';
@@ -8,19 +9,7 @@ type Result = 'Win' | 'Lose' | 'Tie';
 
 const choices: Choice[] = ['Leaf', 'Fire', 'Water'];
 
-const getComputerChoice = (): Choice => choices[Math.floor(Math.random() * choices.length)];
 
-const determineWinner = (playerChoice: Choice, computerChoice: Choice): Result => {
-    if (playerChoice === computerChoice) return 'Tie';
-    if (
-        (playerChoice === 'Fire' && computerChoice === 'Leaf') ||
-        (playerChoice === 'Water' && computerChoice === 'Fire') ||
-        (playerChoice === 'Leaf' && computerChoice === 'Water')
-    ) {
-        return 'Win';
-    }
-    return 'Lose';
-};
 
 interface GameResult {
     playerChoice: Choice;
@@ -29,20 +18,7 @@ interface GameResult {
 }
 
 export default function LeafFireWater() {
-    const [result, setResult] = useState<GameResult | null>(null);
-    const [showPrize, setShowPrize] = useState<boolean>(false);
-    const [showModal, setShowModal] = useState<boolean>(false);
-    const [prizeClaimed, setPrizeClaimed] = useState<boolean>(false);
-
-    const resetGame = () => {
-        setResult(null);
-        setShowPrize(false);
-        setPrizeClaimed(false);
-    };
-
-    const claimPrize = () => {
-        setShowModal(true);
-    };
+    
 
     return (
         <div style={{
@@ -70,7 +46,19 @@ export default function LeafFireWater() {
                 position: 'relative'
             }}>
                 <h1 style={{ fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '2rem', textAlign: 'center' }}>LFW Game Home</h1>
-                <ConnectButton client={client} />
+                <ConnectButton 
+                    client={client} 
+                    wallets={[
+                        inAppWallet({
+                            auth: {
+                                options:[
+                                    "email"
+                                ]
+                            }
+                        })
+                    ]}
+                
+                />
 
                 
 
