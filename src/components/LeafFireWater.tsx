13f6051378ebbc7e15fbfc266650d0688f72af24
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-22 10:38:05 -0800

    🍃 create function claimPrize

--- src/components/LeafFireWater.tsx
+++ src/components/LeafFireWater.tsx
@@ -34,4 +34,8 @@ export default function LeafFireWater() {
         setShowPrize(false);
         setPrizeClaimed(false);
     };
+
+    const claimPrize = () => {
+        setShowModal(true);
+    };
 }
