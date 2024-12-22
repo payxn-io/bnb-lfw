dd571d6481f7fc69f5a66cfe1bac628b3865507a
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-22 10:29:02 -0800

    🍃 create function that randomly chooses from the selection

--- src/components/LeafFireWater.tsx
+++ src/components/LeafFireWater.tsx
@@ -2,3 +2,5 @@ type Choice = 'Leaf' | 'Fire' | 'Water';
 type Result = 'Win' | 'Lose' | 'Tie';
 
 const choices: Choice[] = ['Leaf', 'Fire', 'Water'];
+
+const getComputerChoice = (): Choice => choices[Math.floor(Math.random() * choices.length)];
