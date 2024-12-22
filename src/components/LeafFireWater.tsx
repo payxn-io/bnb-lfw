f3d750c6d98b54dffcf19d3c2db02d5e10aaf995
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-22 11:01:28 -0800

    🍃 add ConnectButton

--- src/components/LeafFireWater.tsx
+++ src/components/LeafFireWater.tsx
@@ -70,9 +70,8 @@ export default function LeafFireWater() {
             }}>
                 <h1 style={{ fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '2rem', textAlign: 'center' }}>LFW Game Home</h1>
                 <ConnectButton
-                        client={client}
-                        
-                    />     
+                    client={client}
+                    />   
             </div>
 
         </div>
