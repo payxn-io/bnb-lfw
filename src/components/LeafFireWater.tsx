3cd8a88f37500a5f4bdb40dd51f5118d928d0f06
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-22 23:19:30 -0800

    🍃 add link to the monke game

--- src/components/LeafFireWater.tsx
+++ src/components/LeafFireWater.tsx
@@ -201,7 +201,7 @@ export default function LeafFireWater() {
                         ))}
                     </div>
                     <h3>New Games:</h3>
-                    <a href="https://google.com" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+                    <a href="mkultrabtc.com/panda2" target="" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
                         <div style={{ textAlign: "center", marginTop: "20px" }}>
                             <img 
                                 src={monke} 
