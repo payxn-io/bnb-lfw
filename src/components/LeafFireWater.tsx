52c0ff97b9b96d2957582aa065f6b2df91084ada
0xGRAV3R <ycopyart.xyz@gmail.com>
2024-12-22 23:13:20 -0800

    🍃 removed newLogo

--- src/components/LeafFireWater.tsx
+++ src/components/LeafFireWater.tsx
@@ -7,7 +7,6 @@ import { getContract } from "thirdweb";
 import { bscTestnet } from "thirdweb/chains";
 import { claimTo, getBalance } from "thirdweb/extensions/erc20";
 import payxnLogo from "../assets/payxn-logo-BNB.png";
-import newLogo from "../assets/payxn-logo.png";
 import monke from "../assets/monke.png";
 import giraffe from "../assets/giraffe.png";
 
